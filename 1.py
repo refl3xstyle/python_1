f0154fe934bad3cc0ffe8d4b97e1c7d8c9230726
Kostya <36366626+refl3xstyle@users.noreply.github.com>
2018-05-14 17:26:04 +0500

    Update 1.py
    fixed bugs..

--- 1.py
+++ 1.py
@@ -5,6 +5,6 @@ year = int(input())
 # Если год не делится на 4 или делится на 100, но не на 400,
 # то он обычный. Во всех остальных случаях - високосный.
 if year % 4 != 0 or (year % 100 == 0 and year % 400 != 0):
-    print("usual year")
+    print("Обычный год")
 else:
-    print("intercalary year")
+    print("Високосный год")
