8848bd30e7a1975fd881463afa35dac1565e2685
dev_koctyj <xaker-pro2013@yandex.ru>
2018-05-14 21:29:02 +0500

    Release ex#9

---
+++ 9.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+mas = np.random.randint(30, size=int(input('Введите количество элементов в массиве: ')))
+
+print('Элементы сгенерированного массива: ')
+for i in range(0,len(mas)):
+    print(mas[i], end=' ')
+
+print('\n')
+print('\nМаксимальный элемент массива: {color}{str}{endcolor}'.format(color = '\x1b[1;33;92m', str = np.amax(mas), endcolor='\x1b[0m'))
+print('\n')
