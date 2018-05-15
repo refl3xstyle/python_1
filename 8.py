0eb860d4bcd30b4e99a91cde2a1bcf698c7c9e21
dev_koctyj <xaker-pro2013@yandex.ru>
2018-05-15 10:50:42 +0500

    Release ex#8, fixed bugs...

--- 8.py
+++ 8.py
@@ -1,7 +1,6 @@
 import numpy as np
 
 mas = np.random.randint(30, size=int(input('Введите количество элементов в массиве: ')))
-
 for i in range(1, len(mas) - 1):
     if ((mas[i-1] > 0) and (mas[i+1] > 0)):
         str = '{color_success}Выполняется!{endcolor}'.format(color_success='\x1b[1;33;92m', endcolor='\x1b[0m')
