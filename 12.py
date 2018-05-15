71fe09daab8f72dd9fcdd83422f0636e7c3e494a
dev_koctyj <xaker-pro2013@yandex.ru>
2018-05-15 20:20:04 +0500

    Release ex#12, fixed minor bugs..

--- 12.py
+++ 12.py
@@ -1,6 +1,6 @@
 source = input('Введите строку (потенциальный палиндром): ')
 output = source[::-1]
-if source == output:
+if source.lower() == output.lower():
   print('{color}Слово является палиндромом!{endcolor}'.format(color = '\x1b[1;33;92m', endcolor='\x1b[0m'))
 else:
   print('{color}Слово НЕ является палиндромом!{endcolor}'.format(color = '\x1b[1;33;93m', endcolor='\x1b[0m'))
