1f993d6cdb09da31e7f631717bf99956cbbfcb32
dev_koctyj <xaker-pro2013@yandex.ru>
2018-05-15 20:26:34 +0500

    Release ex#6, fixed bugs..

--- 6.py
+++ 6.py
@@ -2,8 +2,8 @@ x = int(input('Введите x: '))
 
 i = 1
 print('Все квадраты чисел не превосходящий {}: '.format(x))
-while i**i <= x:
-      print('Квадрат числа {}: = {}'.format(i, i**i))
+while i*i <= x:
+      print('Квадрат числа {}: = {}'.format(i, i*i))
       i += 1
 
 print('\n')
