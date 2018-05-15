f895e8700eac078c835fdfcdd34661502c42d199
dev_koctyj <xaker-pro2013@yandex.ru>
2018-05-15 19:22:44 +0500

    Release ex#8, fixed bugs..

--- 8.py
+++ 8.py
@@ -1,10 +1,13 @@
 import numpy as np
+mas = np.random.randint(-100,100, (int(input('Введите количество элементов в массиве: '))))
+print('Сгенерированные числа массива: ')
+for i in range(0,len(mas)):
+    print(mas[i], end=' ')
 
-mas = np.random.randint(30, size=int(input('Введите количество элементов в массиве: ')))
 for i in range(1, len(mas) - 1):
     if ((mas[i-1] > 0) and (mas[i+1] > 0)):
         str = '{color_success}Выполняется!{endcolor}'.format(color_success='\x1b[1;33;92m', endcolor='\x1b[0m')
     else:
         str = '{color_wrong}Не выполняется!{endcolor}'.format(color_wrong='\x1b[1;33;91m', endcolor='\x1b[0m')
 
-print('Все соседние элементы с одинаковым знаком. Условие: {}'.format(str))
+print('\nВсе соседние элементы с одинаковым знаком. Условие: {}'.format(str))
