db4aa065faeae9c5a48056256223d53b9a14c26a
dev_koctyj <xaker-pro2013@yandex.ru>
2018-05-15 19:46:14 +0500

    Release ex#1, fixed bugs..

--- 1.py
+++ 1.py
@@ -7,17 +7,19 @@ def check_year(year):
 # Выбор варианта рассчета
 try:
     flag = int(input('Выберите из один вариантов программы: \n1 - посмотреть конкретный год\n2 - посмотреть диапозон лет\n'))
-except ValueError:
-    print('Введите только 1 или 2')
+except KeyError as e:
+    raise ValueError('Введите только 1 или 2, ошибка: {}'.format(e.args[0]))
 
 if (flag == 1):
     year = int(input('Введите первое число года: '))
     check_year(year)
-else:
+elif (flag == 2):
     start = int(input('Введите первое число года: '))
-    end = int(input('Введите первое число года: '))
+    end = int(input('Введите второе число года: '))
     for i in range(start,end):
         check_year(i)
+else:
+    print('Выберите только 1 или 2!')
 
 
 
